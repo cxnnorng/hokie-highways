3c454ad674d0ba611fe24836e4b3727daa0fe0fd
AndrewJaeHand <ajhand@vt.edu>
2025-11-06 11:53:47 -0500

    forgot to save

--- frontend/src/pages/FavoriteRoutesPage.jsx
+++ frontend/src/pages/FavoriteRoutesPage.jsx
@@ -1,1 +1,12 @@
 import React, { useEffect, useState } from "react";
+
+const FavoriteRoutesPage = () => {
+return (
+    <div className="">
+      
+    </div>
+  );
+};
+
+
+export default FavoriteRoutesPage;
