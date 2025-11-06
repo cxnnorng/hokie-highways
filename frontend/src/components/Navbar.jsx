be373b64c9eea3cd4118252d55ab28f17bfce3d4
zachary brosius <ztbrosius@gmail.com>
2025-11-06 12:16:17 -0500

    swapped favorite and plan a trip based on ui design

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,36 +9,33 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <nav className="container">
-        <Link to="/" style={{ textDecoration: 'none' }}>
-        <div className="nav-option">
-          <FaMap className="icon" />
-          <span>Map</span>
-        </div>
-        </Link>
-        
-        <div className="divider"></div>
-        <Link to="/favorites" style={{ textDecoration: 'none' }}>
-        <div className="nav-option">
-          <FaRegStar className="icon" />
-          <span>Favorite Routes</span>
-        </div>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <div className="nav-option">
+            <FaMap className="icon" />
+            <span>Map</span>
+          </div>
         </Link>
         <div className="divider"></div>
-        <Link to="/trip" style={{ textDecoration: 'none' }}>
-        <div className="nav-option">
-          <FaRoute className="icon" />
-          <span>Plan a Trip</span>
-        </div>
+        <Link to="/trip" style={{ textDecoration: "none" }}>
+          <div className="nav-option">
+            <FaRoute className="icon" />
+            <span>Plan a Trip</span>
+          </div>
         </Link>
         <div className="divider"></div>
-        <Link to="/settings" style={{ textDecoration: 'none' }}>
-        <div className="nav-option">
-          
-          <BsGear className="icon" />
-          <span>Settings</span>
-        </div>
+        <Link to="/favorites" style={{ textDecoration: "none" }}>
+          <div className="nav-option">
+            <FaRegStar className="icon" />
+            <span>Favorite Routes</span>
+          </div>
         </Link>
-        
+        <div className="divider"></div>
+        <Link to="/settings" style={{ textDecoration: "none" }}>
+          <div className="nav-option">
+            <BsGear className="icon" />
+            <span>Settings</span>
+          </div>
+        </Link>
       </nav>
     </div>
   );
