08e847f71ebcc07128f617374dfd2d28dc97dc4e
colleen23 <colleen23@vt.edu>
2025-11-13 10:22:52 -0500

    test

--- frontend/src/pages/FavoriteRoutesPage.jsx
+++ frontend/src/pages/FavoriteRoutesPage.jsx
@@ -1,8 +1,39 @@
+/**
+ * Author: Colleen Piccolo
+ * Version: 11.13.2025
+ */
+
 import React, { useEffect, useState } from "react";
+
+function buttonFunc() {
+  return;
+}
 
 const FavoriteRoutesPage = () => {
 return (
-    <div className="">
+    <div className="favorite">
+      <div className="route">
+        <button onClick={buttonFunc}>
+          Route 1
+        </button>
+        <button onClick={buttonFunc}>
+          Route 2
+        </button>
+        <button onClick={buttonFunc}>
+          Route 3
+        </button>
+      </div>
+      <div className="bus">
+          <button onClick={buttonFunc}>
+            Route 1
+          </button>
+          <button onClick={buttonFunc}>
+            Route 2
+          </button>
+          <button onClick={buttonFunc}>
+            Route 3
+          </button>
+      </div>
       
     </div>
   );
