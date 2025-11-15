57b3299ada2b701b95cc27a4465c5d7571b65904
colleen23 <colleen23@vt.edu>
2025-11-14 21:12:40 -0500

    FavoriteRoutesPage Updates

--- frontend/src/pages/FavoriteRoutesPage.jsx
+++ frontend/src/pages/FavoriteRoutesPage.jsx
@@ -1,43 +1,72 @@
 /**
  * Author: Colleen Piccolo
- * Version: 11.13.2025
+ * Version: 11.14.2025
  */
 
 import React, { useEffect, useState } from "react";
 
-function buttonFunc() {
-  return;
+/**
+ * The code below was largely sourced from the quick start
+ * react dev site, the page can be found at:  
+ * https://react.dev/learn. 
+ */
+
+const favoriteBusses = [
+  "SMA",
+  "SME",
+  "SMS"
+
+]
+
+const favoriteRoutes = [
+  "Maroon Bay 9 - #8009", 
+  "McBryde Hall - #1142", 
+  "Patrick Henry WestBound - #1516"
+]
+
+// This function gets the status of each bus, this is just
+// a sample output
+
+function getStatus(bus) {
+  return "On Schedule";
 }
 
+// This function gets the status of the route, this is just
+// a sample output
+
+function getRouteStatus(route) {
+  return "On Schedule";
+}
 const FavoriteRoutesPage = () => {
 return (
     <div className="favorite">
-      <div className="route">
-        <button onClick={buttonFunc}>
-          Route 1
-        </button>
-        <button onClick={buttonFunc}>
-          Route 2
-        </button>
-        <button onClick={buttonFunc}>
-          Route 3
-        </button>
-      </div>
       <div className="bus">
-          <button onClick={buttonFunc}>
-            Route 1
-          </button>
-          <button onClick={buttonFunc}>
-            Route 2
-          </button>
-          <button onClick={buttonFunc}>
-            Route 3
-          </button>
+        Bus
+        <li>
+          {favoriteBusses[0]} {getStatus(favoriteBusses[0])}
+        </li> 
+        <li>
+          {favoriteBusses[1]} {getStatus(favoriteBusses[1])}
+        </li>
+        <li>
+          {favoriteBusses[2]} {getStatus(favoriteBusses[2])}
+        </li>
+      </div>
+      <div className="route">
+        Route
+        <li>
+          {favoriteRoutes[0]} {getRouteStatus(favoriteRoutes[0])}
+        </li>
+        <li>
+          {favoriteRoutes[1]} {getRouteStatus(favoriteRoutes[1])}
+        </li>
+        <li>
+          {favoriteRoutes[2]} {getRouteStatus(favoriteRoutes[2])}
+        </li>
+        
       </div>
       
     </div>
   );
 };
-
-
 export default FavoriteRoutesPage;
